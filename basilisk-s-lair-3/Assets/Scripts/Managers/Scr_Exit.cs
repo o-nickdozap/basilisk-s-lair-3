83722ea9e0f507b67fe93f7b332173e9197eb8e2
o-nickdozap <onickdozap2@gmail.com>
2024-07-09 16:50:31 -0300

    Update antes de tudo dar errado

--- basilisk-s-lair-3/Assets/Scripts/Managers/Scr_Exit.cs
+++ basilisk-s-lair-3/Assets/Scripts/Managers/Scr_Exit.cs
@@ -5,18 +5,13 @@ using UnityEngine.SceneManagement;
 public class Scr_Exit : MonoBehaviour
 {
     [SerializeField] Vector2 _starScenePosition;
-    [SerializeField] bool _isReturning;
-    private int _roomIndex;
+    [SerializeField] string _targetRoomName;
 
     void OnCollisionEnter2D(Collision2D _col)
     {
         if (_col.collider.CompareTag("Player")) {
-            if (_isReturning) { _col.gameObject.GetComponent<Scr_PlayerStateManager>().playerDirection *= -1; }
-
-            _roomIndex = _isReturning ? -1 : 1;
-
             _col.gameObject.GetComponent<Scr_PlayerStateManager>()._startScenePosition = _starScenePosition;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + _roomIndex, LoadSceneMode.Single);
+            SceneManager.LoadScene(_targetRoomName, LoadSceneMode.Single);
         }
     }
 }
